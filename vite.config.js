cc58f3f9e372bfbce9bd1059f4db80f8b638ef71
sahil02802 <sahil02817@gmail.com>
2025-12-04 19:48:31 +0530

    react app done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "learn-github-using-react-application",
+  base: "/learn-github-using-react-application/",
   plugins: [react()],
 })
